88da8f30ba933c68693e3c76984f8c5461a530b0
David Webb <dave@davidwebb.me.uk>
2017-12-15 11:53:42 +0000

    Day 15 More Tweaks
    Tidy up a bit

--- day15.py
+++ day15.py
@@ -15,11 +15,16 @@ def pairs(a, b, count):
 def match(pair):
     return pair[0] & MASK == pair[1] & MASK
 
-a = generate(512, 16807)
-b = generate(191, 48271)
-print sum(match(pair) for pair in pairs(a,b,40000000))
+def part1():
+    a = generate(512, 16807)
+    b = generate(191, 48271)
+    print sum(match(pair) for pair in pairs(a,b,40000000))
 
-a = generate(512, 16807, 4)
-b = generate(191, 48271, 8)
-print sum(match(pair) for pair in pairs(a,b,5000000))
+def part2():
+    a = generate(512, 16807, 4)
+    b = generate(191, 48271, 8)
+    print sum(match(pair) for pair in pairs(a,b,5000000))
 
+if __name__ == "__main__":
+    part1()
+    part2()
