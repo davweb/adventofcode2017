08f2cf1363761911ee8b2da33a26d985789ccea7
David Webb <dave@davidwebb.me.uk>
2017-12-18 17:24:23 +0000

    Day 18 Part 2

--- day18.py
+++ day18.py
@@ -64,12 +64,68 @@ def part1(data):
     return None
 
 def part2(data):
-    pass
+    instructions = parse_input(data)
+    all_registers = [defaultdict(int), defaultdict(int)]
+    all_pointers = [0,0]
+    all_queues = [[], []]
+    program = 0
+
+    all_registers[0]['p'] = 0
+    all_registers[1]['p'] = 1
+
+    first = True
+    count = 0
+    
+    while sum(len(q) for q in all_queues) > 0 or first:
+        registers = all_registers[program]
+        pointer = all_pointers[program]
+        queue = all_queues[program]
+        other_queue = all_queues[1 - program]
+
+        command, register, y = instructions[pointer]
+        x = register_value(registers, register)
+        y = register_value(registers, y)
+
+        step = 1
+
+        if command == "snd":
+            first = False            
+            other_queue.append(x)
+            if program == 1:
+                count += 1
+        elif command == "set":
+            registers[register] = y
+        elif command == "add":
+            registers[register] = x + y
+        elif command == "mul":
+            registers[register] = x * y
+        elif command == "mod":
+            registers[register] = x % y
+        elif command == "rcv":
+            if len(queue) == 0:
+                program = 1 - program
+                step = 0
+            else:
+                registers[register] = queue.pop(0)
+        elif command == "jgz":
+            if x > 0:
+                step = y
+        else:
+            raise Exception("Unknown instruction {}".format(command))
+
+        pointer += step
+        all_pointers[program] += step
+
+    return count
+
+
+
 
 def main():
     print part1(file("day18-sample.txt"))
     print part1(file("day18-input.txt"))
-    part2("data")
+    print part2(file("day18-sample2.txt"))
+    print part2(file("day18-input.txt"))
 
 if __name__ == "__main__":
     main()
