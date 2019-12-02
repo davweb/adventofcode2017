68a22244e59b17dfcd7212b04b932d697f51a303
David Webb <dwebb@tripadvisor.com>
2019-12-02 10:26:34 +0000

    2018
    Solve 2018 problems too!

---
+++ 2018/day1.py
@@ -0,0 +1,73 @@
+#!/usr/local/bin/python3
+
+import collections
+import itertools
+
+def read_input():
+    file = open('day1-input.txt', 'r')
+    return [int(line) for line in file.readlines()]
+
+def calculate_frequency(data):
+    """For example, if the device displays frequency changes of +1, -2, +3, +1, then
+    starting from a frequency of zero, the following changes would occur:
+
+    Current frequency  0, change of +1; resulting frequency  1.
+    Current frequency  1, change of -2; resulting frequency -1.
+    Current frequency -1, change of +3; resulting frequency  2.
+    Current frequency  2, change of +1; resulting frequency  3.
+    In this example, the resulting frequency is 3.
+
+    Here are other example situations:
+
+    >>> calculate_frequency([+1, +1, +1])
+    3
+    >>> calculate_frequency([+1, +1, -2])
+    0
+    >>> calculate_frequency([-1, -2, -3])
+    -6
+    """
+
+    return sum(data)
+
+def find_repeat(data):
+    """You notice that the device repeats the same frequency change list over and
+    over. To calibrate the device, you need to find the first frequency it
+    reaches twice.
+
+    >>> find_repeat([+1, -1])
+    0
+    >>> find_repeat([+3, +3, +4, -2, -4])
+    10
+    >>> find_repeat([-6, +3, +8, +5, -6])
+    5
+    >>> find_repeat([+7, +7, -2, -7, -4])
+    14
+    """ 
+
+    count = collections.defaultdict(int)
+    value = 0
+
+    for increment in itertools.cycle(data):
+        count[value] += 1
+
+        if count[value] == 2:
+            return value
+
+        value += increment
+
+
+
+def part1(data):
+    total = calculate_frequency(data)
+    print(total);
+
+def part2(data):
+    print(find_repeat(data))
+
+def main():
+    data = read_input()
+    part1(data)
+    part2(data)
+
+if __name__ == "__main__":
+    main()
---
+++ 2018/day2.py
@@ -0,0 +1,112 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+# To make sure you didn't miss any, you scan the likely candidate boxes again,
+# counting the number that have an ID containing exactly two of any letter and
+# then separately counting those with exactly three of any letter. You can
+# multiply those two counts together to get a rudimentary checksum and compare
+# it to what your device predicts.
+
+# For example, if you see the following box IDs:
+
+# abcdef contains no letters that appear exactly two or three times.
+# bababc contains two a and three b, so it counts for both.
+# abbcde contains two b, but no letter appears exactly three times.
+# abcccd contains three c, but no letter appears exactly two times.
+# aabcdd contains two a and two d, but it only counts once.
+# abcdee contains two e.
+# ababab contains three a and three b, but it only counts once.
+
+import collections
+import itertools
+
+def read_input():
+    file = open('day2-input.txt', 'r')
+    return list(line.strip() for line in file.readlines())
+
+def count_letters(label):
+    """
+    >>> count_letters('aaa')
+    {'a': 3}
+    >>> count_letters('abaa')
+    {'a': 3, 'b': 1}
+    """
+    count = collections.defaultdict(int)
+
+    for letter in label:
+        count[letter] += 1
+
+    return dict(count)
+
+def has_count(label, count):
+    """
+    >>> has_count('abcdef', 2)
+    False
+    >>> has_count('', 2)
+    False
+    >>> has_count('bababc', 2)
+    True
+    >>> has_count('bababc', 3)
+    True
+    >>> has_count('abbcde', 2)
+    True
+    >>> has_count('abcccd', 2)
+    False
+    >>> has_count('aabcdd', 2)
+    True
+    >>> has_count('aabcdd', 3)
+    False
+    >>> has_count('abcdee', 2)
+    True
+    >>> has_count('ababab', 2)
+    False
+    >>> has_count('ababab', 3)
+    True
+    """
+
+    return count in count_letters(label).values()
+
+def different_letter_count(first, second):
+    """
+    >>> different_letter_count('aaaa', 'aaa')
+    Traceback (most recent call last):
+        ...
+    ValueError: labels are not the same length "aaaa", "aaa"
+    >>> different_letter_count('aab', 'aaa')
+    1
+    >>> different_letter_count('abc', 'def')
+    3
+    """
+
+    if len(first) != len(second):
+        raise ValueError('labels are not the same length "{}", "{}"'.format(first, second))
+
+    return sum(a != b for (a,b) in zip(first, second))
+
+def part1(data):
+    twos = 0
+    threes = 0
+    
+    for label in data:
+        if has_count(label, 2):
+            twos += 1
+        if has_count(label, 3):
+            threes += 1
+
+    print(twos * threes)
+
+def part2(data):
+    for (first, second) in itertools.product(data, data):
+        if different_letter_count(first, second) == 1:
+            result = "".join(a for (a, b) in zip(first,second) if a == b)
+            print(result)
+            return
+
+
+def main():
+    data = read_input()
+    part1(data)
+    part2(data)
+
+if __name__ == "__main__":
+    main()
